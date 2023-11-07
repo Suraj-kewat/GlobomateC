cfb6bba8a4bfbf835ec8146c37f6bbd3018fe932
Suraj-kewat <ssiyc936@gmail.com>
2023-11-07 13:31:06 +0530

    Main

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,27 @@
+export const TrendData= [
+  {
+      name: "Gear5-luffy",
+      shares: 980,
+  },
+  {
+      name: "Avengers",
+      shares: 128,
+  },
+  {
+      name: "Chandrayan3",
+      shares: 100,
+  },
+  {
+      name: "Money Heist",
+      shares: 96,
+  },
+  {
+      name: "Roblox",
+      shares: 87,
+  },
+  {
+      name: "Elon Musk",
+      shares: 68,
+  },
+
+];
